7e539b209771a8484a7e612996dec7615ffdc37e
Nikita Pshenov <niaher.crimston@gmail.com>
2011-10-20 08:54:37 +0700

    Fixed the bug with dependent assemblies not loading.

--- Crool.Package/MyControl.xaml.cs
+++ Crool.Package/MyControl.xaml.cs
@@ -1,9 +1,8 @@
 ﻿namespace Microsoft.Crool_Package
 {
+    using System.Windows;
     using System.Windows.Controls;
     using Crool.Business.Managers;
-    using Crool.DataAccess.Repositories;
-    using Crool.Domain.Entities;
 
     /// <summary>
     /// Interaction logic for MyControl.xaml
@@ -14,16 +13,13 @@
         {
             InitializeComponent();
 
-            // For some really strage reason only if we use something from an assembly, will that assembly be loaded.
-            // If we don't have the following 2 lines, at some point we might get "type <something> not found in assembly...".
-            // TODO: Find out why this happens and fix this behavior.
-            new ReviewRepository();
-            new Review();
+            this.dataGrid1.DataContext = new ReviewManager().GetByFileId(1);
+            
         }
 
-        private void button1_Click(object sender, System.Windows.RoutedEventArgs e)
+        private void button1_Click(object sender, RoutedEventArgs e)
         {
-            var fileReviews = new ReviewManager().GetByFileId(1);
+            
         }
     }
 }
